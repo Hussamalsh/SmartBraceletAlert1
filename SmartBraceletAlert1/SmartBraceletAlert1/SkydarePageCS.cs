dfaa0a3985e5bdf3edea4db5f428eac869278c67
davalm14 <davidalmgren95@gmail.com>
2017-01-19 15:15:36 +0100

    skyddare delvis gjort

--- SmartBraceletAlert1/SmartBraceletAlert1/SkydarePageCS.cs
+++ SmartBraceletAlert1/SmartBraceletAlert1/SkydarePageCS.cs
@@ -10,20 +10,59 @@ namespace SmartBraceletAlert1
 {
     public class AboutPageCS : ContentPage
     {
-
+        Label resultsLabel;
+        SearchBar searchBar;
 
 
 
         public AboutPageCS()
         {
-            Title = "Skydare Page";
+            Title = "Skyddare";
+            TableView tableView = new TableView
+            {
+                Intent = TableIntent.Form,
+
+                Root = new TableRoot
+                {
+
+
+                    new TableSection("Här väljer du skyddare")
+                    {
+                        new SwitchCell
+                        {
+                            Text = "Alla användare av den här applikationen:"
+                        }
+                }
+
+            }
+            };
+            Label resultsLabel;
+            resultsLabel = new Label
+            {
+                Text = "Result will appear here.",
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            searchBar = new SearchBar
+            {
+                Placeholder = "Sök personer...",
+                SearchCommand = new Command(() => { resultsLabel.Text = "Result: " + searchBar.Text + " is what you asked for."; })
+            };
+
             Content = new StackLayout
             {
-                Children = {
+                Children =
+                {
+                   tableView,
                     new Label {
-                        Text = "Skydare list data goes here",
-                        HorizontalOptions = LayoutOptions.Center,
-                        VerticalOptions = LayoutOptions.CenterAndExpand
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        Text = "SearchBar",
+                        FontSize = 50
+                    },
+                    searchBar,
+                    new ScrollView
+                    {
+                        Content = resultsLabel,
+                        VerticalOptions = LayoutOptions.FillAndExpand
                     }
                 }
             };
